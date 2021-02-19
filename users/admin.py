d8339c8646f881560c609de3d8067482ee573f10
zpskek <wjdghdwns0@gmail.com>
2021-02-19 21:31:49 +0900

    Create User Model and Admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "gender",
+        "language",
+        "currency",
+        "birthdate",
+        "superhost",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    list_filter = UserAdmin.list_filter + (
+        "gender",
+        "language",
+        "currency",
+        "superhost",
+    )
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "bio",
+                    "gender",
+                    "language",
+                    "currency",
+                    "birthdate",
+                    "superhost",
+                )
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -1,5 +1,35 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+
+    """ Custom User model """
+
+    GENDER_MALE = "Male"
+    GENDER_FEMALE = "Female"
+    GENDER_OTHER = "Other"
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_KOREAN = "KR"
+    LANGUAGE_ENGLISH = "EN"
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_ENGLISH, "English"),
+    )
+
+    CURRENCY_KRW = "KRW"
+    CURRENCY_USD = "USD"
+    CURRENCY_CHOICES = ((CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USD"))
+
+    avatar = models.ImageField(blank=True)
+    bio = models.TextField(blank=True)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, null=True)
+    language = models.CharField(max_length=10, choices=LANGUAGE_CHOICES, null=True)
+    currency = models.CharField(max_length=10, choices=CURRENCY_CHOICES, null=True)
+    birthdate = models.DateField(null=True)
+    superhost = models.BooleanField(default=False)
