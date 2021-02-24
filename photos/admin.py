81dbdb166bf6b33d40cbf20f151fa9bc6c0cba45
zpskek <wjdghdwns0@gmail.com>
2021-02-24 11:23:49 +0900

    Create Photo Admin with mark_safe

--- photos/admin.py
+++ photos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -7,4 +8,13 @@ class PhotoAdmin(admin.ModelAdmin):
 
     """ Photo Admin Definition """
 
-    pass
+    list_display = (
+        "caption",
+        "get_thumbnail",
+        "room",
+    )
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f"<img width='50px' src={obj.file.url}>")
+
+    get_thumbnail.short_description = "Thumbnail"
