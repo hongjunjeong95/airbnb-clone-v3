a38f1c2055bed3da541a11a81e97c3b017fe4e7d
zpskek <wjdghdwns0@gmail.com>
2021-02-25 11:28:10 +0900

    Fix IntegrityError : NOT NULL constraint failed for list generation command by adding get_user function

--- lists/management/commands/seed_lists.py
+++ lists/management/commands/seed_lists.py
@@ -1,7 +1,10 @@
 from random import choice, randint
+
 from django.core.management.base import BaseCommand
 from django.contrib.admin.utils import flatten
+
 from django_seed import Seed
+
 from lists import models as list_models
 from users import models as user_models
 from rooms import models as room_models
@@ -18,17 +21,25 @@ class Command(BaseCommand):
             "--number", type=int, help=f"How many {NAME} do you want to create"
         )
 
+    def get_user(self, users):
+        for user in users:
+            try:
+                pass
+            except Exception:
+                return user
+
     def handle(self, *args, **options):
         number = options.get("number", 1)
         users = user_models.User.objects.all()
         rooms = room_models.Room.objects.all()
+
         seeder = Seed.seeder()
         seeder.add_entity(
             list_models.List,
             number,
             {
                 "name": lambda x: seeder.faker.sentence(),
-                "user": lambda x: choice(users),
+                "user": lambda x: self.get_user(users),
             },
         )
         created_lists = seeder.execute()
