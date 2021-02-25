6c4e26e1651e8e3ad5137e977d519274ac947715
zpskek <wjdghdwns0@gmail.com>
2021-02-25 09:31:18 +0900

    Create custom commands, seed_reviews, which generate many reviews

---
+++ reviews/management/commands/seed_reviews.py
@@ -0,0 +1,38 @@
+import random
+from django.core.management.base import BaseCommand
+from django_seed import Seed
+from reviews import models as review_models
+from users import models as user_models
+from rooms import models as room_models
+
+
+class Command(BaseCommand):
+
+    help = "This command creates many reviews"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--number", type=int, help="How many reviews do you want to create"
+        )
+
+    def handle(self, *args, **options):
+        number = options.get("number", 1)
+        users = user_models.User.objects.all()
+        rooms = room_models.Room.objects.all()
+        seeder = Seed.seeder()
+        seeder.add_entity(
+            review_models.Review,
+            number,
+            {
+                "accuracy": lambda x: random.randint(1, 5),
+                "communication": lambda x: random.randint(1, 5),
+                "cleanliness": lambda x: random.randint(1, 5),
+                "location": lambda x: random.randint(1, 5),
+                "check_in": lambda x: random.randint(1, 5),
+                "value": lambda x: random.randint(1, 5),
+                "user": lambda x: random.choice(users),
+                "room": lambda x: random.choice(rooms),
+            },
+        )
+        seeder.execute()
+        self.stdout.write(self.style.SUCCESS(f"Successfully create {number} reviews"))
