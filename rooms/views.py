988426ef4c63bb78aff5b433bf10c26b4a8b7a2c
zpskek <wjdghdwns0@gmail.com>
2021-02-26 11:00:18 +0900

    homeView by FBV

--- rooms/views.py
+++ rooms/views.py
@@ -1,19 +1,24 @@
-from django.views.generic import ListView
-from . import models
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage
+from . import models as room_models
 
 
-class HomeView(ListView):
-    model = models.Room
-    template_name = "pages/root/home.html"
-    context_object_name = "rooms"
-    paginate_by = 12
-    paginate_orphans = 6
-    ordering = "created"
+def homeView(request):
+    try:
+        # Get list of rooms
+        rooms_list = room_models.Room.objects.all()
 
-    def get_context_data(self):
-        page = int(self.request.GET.get("page", 1))
+        # Get paginator
+        page = int(request.GET.get("page", 1))
         page_sector = (page - 1) // 5
         page_sector = page_sector * 5
-        context = super().get_context_data()
-        context["page_sector"] = page_sector
-        return context
+        paginator = Paginator(rooms_list, 12, orphans=6)
+        rooms = paginator.get_page(page)
+    except EmptyPage:
+        print("Empty page")
+
+    return render(
+        request,
+        "pages/root/home.html",
+        context={"rooms": rooms, "page_sector": page_sector},
+    )
