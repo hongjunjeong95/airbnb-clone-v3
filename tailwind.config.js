735d970c73037c7b5eb675fc6bdb4191fbdcc958
zpskek <wjdghdwns0@gmail.com>
2021-02-28 15:20:31 +0900

    Apply CSS on home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        18: "4.5rem",
+      },
+      gridAutoRows: {
+        "200px": "minmax(2, 200px)",
+        "300px": "minmax(0, 300px)",
+        "400px": "minmax(0, 400px)",
+      },
+      gridTemplateRows: {
+        "2-200px": "repeat(2, 200px)",
+        "2-400px": "repeat(2, 400px)",
+        "2-300px": "repeat(2, 300px)",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
