b0ce5644d358bbfc8ad061f9d48a796f42dd99dc
zpskek <wjdghdwns0@gmail.com>
2021-04-24 22:28:15 +0900

    Create sample room images

--- config/settings.py
+++ config/settings.py
@@ -148,11 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / "static"]
-else:
-    STATIC_ROOT = BASE_DIR / "static"
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 AUTH_USER_MODEL = "users.User"
 
